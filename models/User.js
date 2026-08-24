16834f4a014bee7e9862580a282c2ceacf9c0b3e
Andrew Killian <ack97@txstate.edu>
2026-08-24 00:51:15 -0400

    fixed user

--- models/User.js
+++ models/User.js
@@ -1,10 +1,24 @@
 import { DataTypes } from "sequelize";
+import bcrypt from "bcrypt";
 import sequelize from "./index.js";
 
-const User = sequelize.define("user", {
-  name: DataTypes.STRING,
-  email: DataTypes.STRING,
-  password: DataTypes.STRING,
-});
+const User = sequelize.define(
+  "user",
+  {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+  },
+  {
+    hooks: {
+      beforeCreate: async (user) => {
+        user.password = await bcrypt.hash(user.password, 10);
+      },
+    },
+    defaultScope: {
+      attributes: { exclude: ["password"] },
+    },
+  }
+);
 
 export default User;
